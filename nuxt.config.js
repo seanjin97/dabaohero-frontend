c622235e7cb643c94bad5d36a50a79d0b7d23576
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2021-09-22 11:28:37 +0800

    update axios base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL,
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
