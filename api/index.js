e29184266ddfd5132311c5ebd32b60393a47f729
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2021-10-13 21:38:48 +0800

    comment vercel.json

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
-const axios = require('axios');
-const express = require('express');
+import express from 'express';
+import axios from 'axios';
+
+// const requireFromUrl = require('require-from-url/sync');
+
+// const axios = requireFromUrl('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
 
 const app = express();
 app.use(express.json());
@@ -10,35 +14,33 @@ const clientSecret = process.env.MGMT_AUTH_CLIENT_SECRET;
 app.post('/', async (req, res, next) => {
   const { body: { email } } = req;
 
-  // try {
-  //   const { data: { access_token, token_type } } = await axios.post(`${process.env.AUTH_URL}/oauth/token`, {
-  //     grant_type: 'client_credentials',
-  //     client_id: clientId,
-  //     client_secret: clientSecret,
-  //     audience: `${process.env.AUTH_URL}/api/v2/`,
-  //   });
+  try {
+    const { data: { access_token, token_type } } = await axios.post(`${process.env.AUTH_URL}/oauth/token`, {
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+      audience: `${process.env.AUTH_URL}/api/v2/`,
+    });
 
-  //   try {
-  //     const options = {
-  //       method: 'GET',
-  //       url: `${process.env.AUTH_URL}/api/v2/users`,
-  //       params: { q: `email:"${email}"`, search_engine: 'v3' },
-  //       headers: { authorization: `${token_type} ${access_token}` },
-  //     };
+    try {
+      const options = {
+        method: 'GET',
+        url: `${process.env.AUTH_URL}/api/v2/users`,
+        params: { q: `email:"${email}"`, search_engine: 'v3' },
+        headers: { authorization: `${token_type} ${access_token}` },
+      };
 
-  //     const { data } = await axios.request(options);
+      const { data } = await axios.request(options);
 
-  //     res.json(await data[0]);
-  //   } catch (err) {
-  //     console.log(err);
-  //     next(err);
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   next(err);
-  // }
-
-  res.json({ username: 'hello' });
+      res.json(await data[0]);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 export default { path: '/api', handler: app };
