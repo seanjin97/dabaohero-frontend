80fb6c0dabc91c044970619960efe76ffcdc55f3
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2021-10-13 20:16:45 +0800

    attempt to reduce lambda api function size

--- api/index.js
+++ api/index.js
@@ -10,33 +10,23 @@ const clientSecret = process.env.MGMT_AUTH_CLIENT_SECRET;
 app.post('/', async (req, res, next) => {
   const { body: { email } } = req;
 
-  try {
-    const { data: { access_token, token_type } } = await axios.post(`${process.env.AUTH_URL}/oauth/token`, {
-      grant_type: 'client_credentials',
-      client_id: clientId,
-      client_secret: clientSecret,
-      audience: `${process.env.AUTH_URL}/api/v2/`,
-    });
+  const { data: { access_token, token_type } } = await axios.post(`${process.env.AUTH_URL}/oauth/token`, {
+    grant_type: 'client_credentials',
+    client_id: clientId,
+    client_secret: clientSecret,
+    audience: `${process.env.AUTH_URL}/api/v2/`,
+  });
 
-    try {
-      const options = {
-        method: 'GET',
-        url: `${process.env.AUTH_URL}/api/v2/users`,
-        params: { q: `email:"${email}"`, search_engine: 'v3' },
-        headers: { authorization: `${token_type} ${access_token}` },
-      };
+  const options = {
+    method: 'GET',
+    url: `${process.env.AUTH_URL}/api/v2/users`,
+    params: { q: `email:"${email}"`, search_engine: 'v3' },
+    headers: { authorization: `${token_type} ${access_token}` },
+  };
 
-      const { data } = await axios.request(options);
+  const { data } = await axios.request(options);
 
-      res.json(await data[0]);
-    } catch (err) {
-      console.log(err);
-      next(err);
-    }
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
+  res.json(await data[0]);
 });
 
 export default { path: '/api', handler: app };
