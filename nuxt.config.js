2a281e49f4ebe3cbb1bf1b2e94403977e9e6aeef
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2021-10-13 12:51:04 +0800

    remove socket.io, add fireabse realtime db

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
     '@chakra-ui/nuxt',
     '@nuxtjs/emotion',
     '@nuxtjs/auth-next',
-    'nuxt-socket-io',
+    '@nuxtjs/firebase',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -91,13 +91,21 @@ export default {
   router: {
     middleware: ['auth'],
   },
-  io: {
-    sockets: [{
-      name: 'main',
-      url: process.env.SOCKET_URL,
-    }],
-  },
   serverMiddleware: [
     '~/api/management',
   ],
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DB_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+    services: {
+      database: true, // this is the realtime database service
+    },
+  },
 };
--- pages/sessions.vue
+++ pages/sessions.vue
@@ -1,38 +1,39 @@
 <template>
   <c-box>
     <c-box>sessions page</c-box>
-    <c-button @click="sendMessage">send message</c-button>
+    <c-box>Hello {{ username }}</c-box>
+    <c-button @click="joinChat">Join Chat</c-button>
   </c-box>
 </template>
 
 <script>
 export default {
+  methods: {
+    async joinChat() {
+      const messageRef = this.$fire.database.ref('test');
+      try {
+        // await messageRef.set({
+        //   name: this.username,
+        //   message: 'Nuxt-Fire with Firebase Realtime Database rocks!',
+        // });
+        await messageRef.push({ name: this.username, message: 'hello' });
+      } catch (e) {
+        alert(e);
+      }
+    },
+  },
   data() {
     return {
-      socket: null,
+      username: '',
     };
   },
-  mounted() {
-    this.socket = this.$nuxtSocket({
-      channel: '/',
-      persist: 'socket',
-    });
-    // join rooms
-    // code here
-    // https:// dev.to/richardeschloss/nuxt-socket-io-how-to-create-a-stealth-mode-chat-application-in-under-10-minutes-1m2g
-    /* Listen for events: */
-    this.socket.on('message', (msg) => {
-      /* Handle event */
-      console.log(msg);
-    });
-  },
-  methods: {
-    async sendMessage() {
-      this.socket.emit('sendMessage', {
-        name: await this.$auth.$storage.getUniversal('username'),
-        message: 'hello world',
-      });
-    },
+  async fetch() {
+    this.username = await this.$auth.$storage.getUniversal('username');
+
+    const messsageRef = this.$fire.database.ref('channels');
+    console.log(messsageRef);
+    const test = await fetch(`${messsageRef.toString()}.json`);
+    console.log(test);
   },
 };
 </script>
