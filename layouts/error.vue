cac0c06bc152e3f117028e9238d5f7d5ca4e3c69
jaytoo22 <jaytoo222@gmail.com>
2021-10-31 01:48:07 +0800

    resetting local files and error page

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <br>
+    <p class="text-primary"> Click  <NuxtLink to="/"> here</NuxtLink> to return to Home page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error',
+};
+</script>
