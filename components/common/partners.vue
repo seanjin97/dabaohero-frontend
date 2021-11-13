d392e9cb6ef9440fb689b2dd0aa0ad2d2a8401db
jaytoo <jaytoo22@outlook.com>
2021-11-14 03:36:23 +0800

    update partners

--- components/common/partners.vue
+++ components/common/partners.vue
@@ -6,7 +6,7 @@
       <div class="row">
 
         <div class="col-lg-3 col-md-6 col-6 d-flex align-items-center justify-content-center">
-        <img src="./img/partners/SGUnited Logo.png" class="img-fluid" style="opacity:80%">
+        <a style="text-align: center" href="https://www.sgunited.gov.sg/"><img src="./img/partners/SGUnited Logo.png" class="img-fluid justify-content-center" style="opacity:80%"></a>
         </div>
 
         <div class="col-lg-3 col-md-6 col-6 d-flex align-items-center justify-content-center">
@@ -18,7 +18,7 @@
         </div>
 
         <div class="col-lg-3 col-md-6 col-6 d-flex align-items-center justify-content-center">
-        <img src="./img/partners/bettersg.png" class="img-fluid" style="opacity:80%">
+        <a style="text-align: center" href="https://better.sg/"><img src="./img/partners/bettersg.png" class="img-fluid" style="opacity:80%"></a>
         </div>
 
       </div>
